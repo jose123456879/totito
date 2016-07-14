6037b02171605c7370584059d97e5213810f5f0f
jose123456879 <totitosst@gmail.com>
2016-07-14 08:44:29 -0600

    JAJAJ

--- totito.py
+++ totito.py
@@ -7,93 +7,96 @@ F="F"
 G="G"
 H="H"
 I="I"
+turnos=1
 win=False
-TABLERO=(A+"_| "+B+"_|_"+C+"\n"+D+"_| "+E+"_|_"+F+"\n"+G+" | "+H+" | "+I)
 print("bienvenidos a totito")
-while win ==False:
-	print("turno P1")
-	print(TABLERO)
-	jugador1=input("ingrese una casilla: ")
-	if jugador1==A and A!="0":
-		A="X"
-	if jugador1==B and B!="o":
-		B="X"
-	if jugador1==C and C!="o":
-		C="X"
-	if jugador1==D and D!="o":
-		D="X"
-	if jugador1==E and E!="o":
-		E="X"
-	if jugador1==F and F!="o":
-		F="X"	
-	if jugador1==G and G!="o":
-		F="X"
-	if jugador1==H and H!="o":
-		H="X"
-	if jugador1==I and I!="o":
-		I="X"
-	if A=="X" and B=="X" and C=="X":
-		win=True
-		print("ganador")
-	if D=="X" and E=="X" and F=="X":
-		win=True
-		print("ganador")
-	if G=="X" and H=="X" and I=="X":
-		win=True
-		print("ganador")
-	if A=="X" and D=="X" and G=="X":
-		win=True
-		print("ganador")
-	if B=="X" and E=="X" and H=="X":
-		win=True
-		print("ganador")
-	if A=="X" and E=="X" and I=="X":
-		win=True
-		print("ganador")
-	if G=="X" and E=="X" and C=="X":
-		win=True
-		print("ganador")
-	print(TABLERO)
-	print("Turno P2")
-	jugador2=input("ingrese una casilla: ")
-	if jugador2==A and A!="X":
-		A="o"		
-	if jugador2==B and B!="X":
-		B="o"
-	if jugador2==C and C!="X":
-		C="o"	
-	if jugador2==D and D!="X":
-		D="o"						
-	if jugador2==E and E!="X":
-		E="o"			
-	if jugador2==F and F!="X":
-		F="o"		
-	if jugador2==G and G!="X":
-		G="o"	
-	if jugador2==H and H!="X":
-		H="o"
-	if jugador2==I and I!="X":
-		I="o"	
-	if A=="X" and B=="X" and C=="X":
-		win=True
-		print("ganador")
-	if D=="X" and E=="X" and F=="X":
-		win=True
-		print("ganador")
-	if G=="X" and H=="X" and I=="X":
-		win=True
-		print("ganador")
-	if A=="X" and D=="X" and G=="X":
-		win=True
-		print("ganador")
-	if B=="X" and E=="X" and H=="X":
-		win=Truprint("ganador")
-	if A=="X" and E=="X" and I=="X":
-		win=True
-		print("ganador")
-	if G=="X" and E=="X" and C=="X":
-		win=True
-		print("ganador")			
-	print(TABLERO)	 
-win==True
-print("empate")
+for judio in range (5):
+	if win ==False and turnos <=5:
+		print(A+"_|_"+B+"_|_"+C+"\n"+D+"_|_"+E+"_|_"+F+"\n"+G+" | "+H+" | "+I)
+		print("turno J1")
+		jugador1=input("ingrese una casilla: ")
+		if jugador1==A and A!="O":
+			A="X"
+		if jugador1==B and B!="O":
+			B="X"
+		if jugador1==C and C!="O":
+			C="X"
+		if jugador1==D and D!="O":
+			D="X"
+		if jugador1==E and E!="O":
+			E="X"
+		if jugador1==F and F!="O":
+			F="X"	
+		if jugador1==G and G!="O":
+			G="X"
+		if jugador1==H and H!="O":
+			H="X"
+		if jugador1==I and I!="O":
+			I="X"
+		if A=="X" and B=="X" and C=="X":
+			win=True
+			print("ganador J1")
+		if D=="X" and E=="X" and F=="X":
+			win=True
+			print("ganador J1")
+		if G=="X" and H=="X" and I=="X":
+			win=True
+			print("ganador J1")
+		if A=="X" and D=="X" and G=="X":
+			win=True
+			print("ganador J1")
+		if B=="X" and E=="X" and H=="X":
+			win=True
+			print("ganador J1")
+		if A=="X" and E=="X" and I=="X":
+			win=True
+			print("ganador J1")
+		if G=="X" and E=="X" and C=="X":
+			win=True
+			print("ganador")
+		if win==False and turnos<=4:	
+			print(A+"_|_"+B+"_|_"+C+"\n"+D+"_|_"+E+"_|_"+F+"\n"+G+" | "+H+" | "+I)
+			print("Turno J2")
+			jugador2=input("ingrese una casilla: ")
+			jugador2=="O"
+			if jugador2==A and A!="X":
+				A="O"		
+			if jugador2==B and B!="X":
+				B="O"
+			if jugador2==C and C!="X":
+				C="O"	
+			if jugador2==D and D!="X":
+				D="O"						
+			if jugador2==E and E!="X":
+				E="O"			
+			if jugador2==F and F!="X":
+				F="O"		
+			if jugador2==G and G!="X":
+				G="O"	
+			if jugador2==H and H!="X":
+				H="O"
+			if jugador2==I and I!="X":
+				I="O"	
+			if A=="O" and B=="O" and C=="O":
+				win=True
+				print("ganador J2")
+			if D=="O" and E=="O" and F=="O":
+				win=True
+				print("ganado J2")
+			if G=="O" and H=="O" and I=="O":
+				win=True
+				print("ganado J2")
+			if A=="O" and D=="O" and G=="O":
+				win=True
+				print("ganador J2")
+			if B=="O" and E=="O" and H=="O":
+				win=True
+				print("ganador J2")
+			if A=="O" and E=="O" and I=="O":
+				win=True
+				print("ganador J2")
+			if G=="O" and E=="O" and C=="O":
+				win=True
+				print("ganador J2")				 
+			if judio>5:
+				print("EMPATE")
